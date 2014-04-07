de877051d2fae44b27c3f8bee30113eb83bd8b35
Elvis Oliveira <elvis.olv@gmail.com>
2014-04-07 19:07:20 -0300

    Added contact details pannel

--- src/main/java/com/elvisoliveira/yellowpages/ui/mainWindow.java
+++ src/main/java/com/elvisoliveira/yellowpages/ui/mainWindow.java
@@ -2,11 +2,17 @@ package com.elvisoliveira.yellowpages.ui;
 
 import com.elvisoliveira.yellowpages.beans.contactbean;
 import com.elvisoliveira.yellowpages.webservice.telelistas;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -15,6 +21,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 import javax.swing.SwingWorker;
 import javax.swing.table.DefaultTableModel;
 import net.miginfocom.swing.MigLayout;
@@ -28,9 +35,11 @@ public class mainWindow {
     private static JTextField searchInput;
     private static JButton searchButton;
     private static List<String> strings;
+    private static ArrayList<Map> contactsArray;
 
     private static final JFrame window = new JFrame("YellowPages");
     private static final JPanel panel = new JPanel();
+    private static final JPanel panelContact = new JPanel();
 
     // format the loading image with it's full path
     static final String ajaxLoader = String.format(
@@ -58,22 +67,7 @@ public class mainWindow {
         searchButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent evt) {
-                // set the loading screen, while the SwingWorker class is working
-                contactsListing.setViewportView(new JLabel("loading... ", new ImageIcon(ajaxLoader), JLabel.CENTER));
-                // get the inputted name
-                final String name = searchInput.getText();
-                // set the SwingWorker class as a buffer streammer
-                SwingWorker<Void, Void> swingWorker = new SwingWorker<Void, Void>() {
-                    @Override // required implementation
-                    public Void doInBackground() throws IOException {
-                        // this will be executed in background
-                        changeContacts(name);
-                        // return anything
-                        return null;
-                    }
-                };
-                // make the swingWorker work
-                swingWorker.execute();
+                searchButton();
             }
         });
         // position the searchButton in MigLayout
@@ -85,9 +79,15 @@ public class mainWindow {
         // the name is not set, give the user instructions to search
         contactsListing.setViewportView(new JLabel("Search a contact", JLabel.CENTER));
         // set the widget dimentions, required to define layout properties
-        contactsListing.setPreferredSize(new Dimension(400, 500));
+        contactsListing.setPreferredSize(new Dimension(400, 300));
         // position the scroll panel in MigLayout
-        panel.add(contactsListing);
+        panel.add(contactsListing, "wrap");
+        
+        // panel of the contact
+        panelContact.setLayout(new MigLayout());
+        panelContact.setPreferredSize(new Dimension(400, 100));
+        
+        panel.add(panelContact, "growx");
 
         // window configuration
         window.add(panel);
@@ -98,7 +98,25 @@ public class mainWindow {
 
     }
 
-    @SuppressWarnings("empty-statement")
+    public static void searchButton() {
+        // set the loading screen, while the SwingWorker class is working
+        contactsListing.setViewportView(new JLabel("loading... ", new ImageIcon(ajaxLoader), JLabel.CENTER));
+        // get the inputted name
+        final String name = searchInput.getText();
+        // set the SwingWorker class as a buffer streammer
+        SwingWorker<Void, Void> swingWorker = new SwingWorker<Void, Void>() {
+            @Override // required implementation
+            public Void doInBackground() throws IOException {
+                // this will be executed in background
+                changeContacts(name);
+                // return anything
+                return null;
+            }
+        };
+        // make the swingWorker work
+        swingWorker.execute();
+    }
+
     public static void changeContacts(String name) throws IOException {
         // make the request and return the Document
         Document document = telelistas.generateDocument(name);
@@ -113,6 +131,7 @@ public class mainWindow {
 
             table = new JTable();
             
+            contactsArray = new ArrayList<>();
             contactsTable = new DefaultTableModel() {
                 @Override
                 public boolean isCellEditable(int row, int column) {
@@ -123,14 +142,51 @@ public class mainWindow {
             contactsTable.addColumn("Endereço");
 
             for (contactbean object : contactsList) {
+                
+                Map<String, String> map = new HashMap<>();
+
+                map.put("name", object.getName());
+                map.put("address", object.getAddress());
+                map.put("link", object.getLink());
+                
+                contactsArray.add(map);
+                
                 contactsTable.addRow(new Object[]{object.getName(), object.getAddress().trim()});
             }
 
+            table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+            table.addMouseListener(new MouseListener() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    selectContact(contactsArray.get(table.getSelectedRow()));
+                }
+
+                @Override
+                public void mousePressed(MouseEvent e) {
+                }
+
+                @Override
+                public void mouseReleased(MouseEvent e) {
+                }
+
+                @Override
+                public void mouseEntered(MouseEvent e) {
+                }
+
+                @Override
+                public void mouseExited(MouseEvent e) {
+                }
+            });
+            
             table.setModel(contactsTable);
 
             contactsListing.setViewportView(table);
         }
 
+    }
+    
+    public static void selectContact(Map info) {
+        panelContact.add(new JLabel("loading... ", new ImageIcon(ajaxLoader), JLabel.CENTER), "wrap");
     }
 
 }
